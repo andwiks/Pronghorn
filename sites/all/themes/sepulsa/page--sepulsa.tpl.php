5bebadb624f54ed3d8d150fb991a4117e1c47934
Abi أب <el@elabee.me>
2015-03-09 12:57:28 +0700

    Add Front page banner region

--- sites/all/themes/sepulsa/page--sepulsa.tpl.php
+++ sites/all/themes/sepulsa/page--sepulsa.tpl.php
@@ -12,7 +12,7 @@ global $base_url;
 ?>
 <div id="page-wrapper">
   <?php include "header.tpl.php"; ?>
-        
+
   <section id="content">
     <div class="container">
       <div id="main">
@@ -29,39 +29,17 @@ global $base_url;
                   <?php print render($page['content']); ?>
                 </div>
               </div>
-            </div>                        
-          </div>
-              
-          <div class="col-sm-6 col-md-4">
-            <div class="post-slider style2 owl-carousel box">
-              <a href="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner1.jpg" class="soap-mfp-popup">
-                <img src="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner1.jpg" alt="">
-                <div class="slide-text">
-                  <h4 class="slide-title">Isi pulsa sekarang gratis</h4>
-                  <span class="meta-info">Dapatkan cashback dengan isi ulang di SEPULSA</span>
-                </div>
-              </a>
-              <a href="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner2b.jpg" class="soap-mfp-popup">
-                <img src="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner2b.jpg" alt="">
-                <div class="slide-text">
-                  <h4 class="slide-title">Temukan deal-deal menarik hanya di SEPULSA</h4>
-                  <span class="meta-info"></span>
-                </div>
-              </a>
-              <a href="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner3.jpg" class="soap-mfp-popup">
-                <img src="<?php print $base_url . '/' . path_to_theme(); ?>/images/banner3.jpg" alt="">
-                <div class="slide-text">
-                  <h4 class="slide-title">Isi pulsa paling gampang ? Ya di SEPULSA</h4>
-                  <span class="meta-info">Juga cepat, aman dan menguntungkan</span>
-                </div>
-              </a>
             </div>
+          </div>
+
+          <div class="col-sm-6 col-md-4">
+            <?php print render($page['banner']); ?>
           </div>
         </div>
       </div>
     </div>
   </section>
-        
+
   <?php include "footer.tpl.php"; ?>
-    
+
 </div>

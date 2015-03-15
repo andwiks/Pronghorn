e480c6ad53181133381b11d1631a50a9684c42a3
Tri <tri@reebonz.com>
2015-03-15 12:57:07 +0700

    override userpoints layout in user detail

---
+++ sites/all/modules/custom/userpoints_sepulsa/userpoints_list_transaction.tpl.php
@@ -0,0 +1,28 @@
+<?php //print "<pre>".print_r($content, true)."</pre>"; ?>
+<table class="table style1">
+    <thead>
+        <tr>
+            <?php //foreach($content['header'] as $header) { ?>
+            <th>Sepulsa Kredit</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Status</th>
+            <?php //} ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+        if (isset($content['rows'])) {
+        foreach($content['rows'] as $rows) { ?>
+        <tr>
+            <?php //foreach($rows['data'] as $rows_value) { ?>
+            <td><?php print number_format($rows['data'][0]['data'],0,",",".") ?></td>
+            <td><?php print $rows['data'][1]['data'] ?></td>
+            <td><?php print $rows['data'][2]['data'] ?></td>
+            <td><?php print $rows['data'][3]['data'] ?></td>
+            <?php //} ?>
+        </tr>
+        <?php } 
+        }?>
+    </tbody>
+</table>

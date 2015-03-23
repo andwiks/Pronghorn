91f9244dbf603fce746e57c8cd3ca8eb3d7d95c2
Abi أب <el@elabee.me>
2015-03-23 11:08:23 +0700

    Theming alert message

--- sites/all/themes/sepulsa/template.php
+++ sites/all/themes/sepulsa/template.php
@@ -375,6 +375,58 @@ function sepulsa_form_element($variables) {
 }
 
 /**
+ * Overrides theme_status_messages().
+ */
+function sepulsa_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+    'info' => t('Informative message'),
+  );
+
+  // Map Drupal message types to their corresponding Bootstrap classes.
+  // @see http://twitter.github.com/bootstrap/components.html#alerts
+  $status_class = array(
+    'status' => 'success',
+    'error' => 'error',
+    'warning' => 'notice',
+    // Not supported, but in theory a module could send any type of message.
+    // @see drupal_set_message()
+    // @see theme_status_messages()
+    'info' => 'help',
+  );
+
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $class = (isset($status_class[$type])) ? ' alert-' . $status_class[$type] : '';
+    $output .= "<div class=\"alert alert-block$class\">\n";
+    // $output .= "  <a class=\"close\" data-dismiss=\"alert\" href=\"#\">&times;</a>\n";
+    $output .= "  <a class=\"close\" data-dismiss=\"alert\" href=\"#\"></a>\n";
+
+    if (!empty($status_heading[$type])) {
+      $output .= '<h4 class="element-invisible">' . $status_heading[$type] . "</h4>\n";
+    }
+
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+
+    $output .= "</div>\n";
+  }
+  return $output;
+}
+
+/**
  * Implements hook_theme().
  */
 function sepulsa_theme($existing, $type, $theme, $path) {

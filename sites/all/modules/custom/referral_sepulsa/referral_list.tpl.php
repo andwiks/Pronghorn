00952f1d2e34868d3497df8be2fe75bc9b58b1de
Tri <tri@reebonz.com>
2015-03-29 22:54:35 +0700

    fix some bugs

--- sites/all/modules/custom/referral_sepulsa/referral_list.tpl.php
+++ sites/all/modules/custom/referral_sepulsa/referral_list.tpl.php
@@ -2,13 +2,13 @@
     <thead>
         <tr>
             <th>User</th>
-            <th>Date</th>
+            <th>Date Join</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($result as $data) { ?>
         <tr>
-            <td><?php print l($data->name, "user/$data->uid") ?></td>
+            <td><?php print l($data->mail, "user/$data->uid") ?></td>
             <td><?php print format_date($data->created, 'custom', REFERRAL_DATE_FORMAT) ?></td>
         </tr>
         <?php } ?>

041b24fb85e82642340169e7a2b0f8f67de8907f
Tri <tri@reebonz.com>
2015-06-16 23:10:14 +0700

    fix the tpl

--- sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
+++ sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
@@ -6,15 +6,15 @@
   });
 </script>
 <div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #eee; z-index: 101; text-align: center;">
-  <table style="text-align: center; margin-left: 30%; width: 50%">
+  <table style="text-align: center; width: 100%">
 	<tr>
-	  <td width="50px"><img width="50px" src="<?php print $content['image_path'] ?>" /></td>
-	  <td width="30%">
+	  <td width="10%"><img width="50px" src="<?php print $content['image_path'] ?>" /></td>
+	  <td width="50%">
 		Sepulsa.com<br />
 		PT. Sepulsa Technology<br />
 		FREE - In Google Play</td>
-	  <td><a href="<?php print $content['apps_path'] ?>"><button class="btn style1">View</button></a>
-		  <button id="close_banner" class="btn style1">Close</button></td>
+	  <td width="40%"><a href="<?php print $content['apps_path'] ?>"><button class="">View</button></a>
+		  <button id="close_banner" class="">Close</button></td>
 	</tr>
   </table>
  </div>

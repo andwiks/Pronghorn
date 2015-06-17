a8621fa312eb8a3ee3e59546d48d62699ca7e326
Tri <tri@reebonz.com>
2015-06-17 11:24:41 +0700

    change some style

--- sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
+++ sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
@@ -5,15 +5,15 @@
 	});
   });
 </script>
-<div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #eee; z-index: 101; text-align: center;">
+<div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #eee; z-index: 101; text-align: center; padding: 10px;">
   <table style="text-align: center; width: 100%">
 	<tr>
 	  <td width="10%"><img width="50px" src="<?php print $content['image_path'] ?>" /></td>
-	  <td width="50%" style="text-align: left; padding-left: 10px;">
+	  <td width="30%" style="text-align: left; padding-left: 10px;">
 		Sepulsa.com<br />
 		PT. Sepulsa Technology<br />
 		FREE - In Google Play</td>
-	  <td width="40%"><a href="<?php print $content['apps_path'] ?>"><button class="">View</button></a>
+	  <td width="50%"><a href="<?php print $content['apps_path'] ?>"><button class="">View</button></a>
 		  <button id="close_banner" class="">Close</button></td>
 	</tr>
   </table>

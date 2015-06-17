271bd0da30dc9821481ba24515fd0ea5db9b2593
Tri <tri@reebonz.com>
2015-06-17 15:57:51 +0700

    change some style

--- sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
+++ sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
@@ -43,10 +43,10 @@
 	});
   });
 </script>
-<div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #D3D3D3; z-index: 101; text-align: center; padding-bottom: 10px;">
+<div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #787878; z-index: 101; text-align: center; padding-bottom: 10px;">
   <table style="text-align: center; width: 100%">
 	<tr>
-	  <td width="20%"><img width="70px" src="<?php if (isset($content['image_path'])) print $content['image_path'] ?>" /></td>
+	  <td width="20%"><img width="90px" src="<?php if (isset($content['image_path'])) print $content['image_path'] ?>" /></td>
 	  <td width="50%" style="text-align: left; padding-left: 10px;">
 		<?php if (isset($content['apps_desc'])) print $content['apps_desc']  ?>
 	  </td>

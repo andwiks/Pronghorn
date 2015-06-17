a94e3c47c6e9d13bf1ecb77bebe6551c5c9bfe0e
Tri <tri@reebonz.com>
2015-06-17 15:08:55 +0700

    fix some layout

--- sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
+++ sites/all/modules/custom/mobile_banner_install/mobile_banner_install.tpl.php
@@ -8,12 +8,11 @@
 <div id="mobile_banner" style="position: absolute; top: -1px; width: 100%; background-color: #eee; z-index: 101; text-align: center; padding-bottom: 10px;">
   <table style="text-align: center; width: 100%">
 	<tr>
-	  <td width="10%"><img width="50px" src="<?php print $content['image_path'] ?>" /></td>
+	  <td width="10%"><img width="50px" src="<?php if (isset($content['image_path'])) print $content['image_path'] ?>" /></td>
 	  <td width="30%" style="text-align: left; padding-left: 10px;">
-		Sepulsa.com<br />
-		PT. Sepulsa Technology<br />
-		FREE - In Google Play</td>
-	  <td width="50%"><a href="<?php print $content['apps_path'] ?>"><button class="btn">View</button></a>
+		<?php if (isset($content['apps_desc'])) print $content['apps_desc']  ?>
+	  </td>
+	  <td width="50%"><a href="<?php if (isset($content['apps_path'])) print $content['apps_path'] ?>"><button class="btn">View</button></a>
 		  <button id="close_banner" class="btn">Close</button></td>
 	</tr>
   </table>

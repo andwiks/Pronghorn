82289af780ae7964e2ab65aa8c362cf035d0b1f8
Dwi Susanto <kansertjr@gmail.com>
2015-08-13 03:41:03 +0700

    one block page

--- sites/all/themes/sepulsav2/page.tpl.php
+++ sites/all/themes/sepulsav2/page.tpl.php
@@ -7,38 +7,28 @@
  * @since July 23th 2015.
  */
 ?>
-<?php if ($node->type != 'blog'): ?>
+<?php if ($node->type == 'campaign_page'): ?>
 <section class="std_content">
   <div class="wrapper_2 other after_clear">
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <div class="c_left">
-        <?php print render($page['sidebar_first']); ?>
-      </div>
+    <?php print $messages; ?>
+    <?php if ($title): ?>
+      <h2><?php print $title ?></h2>
     <?php endif; ?>
 
-    <div class="c_right">
-      <?php if ($title): ?>
-        <h2><?php print $title ?></h2>
-      <?php endif; ?>
-
-      <?php print $messages; ?>
-      <?php print render($page['content']); ?>
-    </div>
+    <?php print render($page['content']); ?>
   </div>
 </section>
-<?php else: ?>
+<?php elseif ($node->type == 'blog') : ?>
 <section id="frameBlog">
     <div class="wrapper">
         
         <div id="inner" class="clearfix">
-            <div id="left_column">                               
+            <div id="left_column">      
+                <?php print $messages; ?>                         
                 <?php if ($title): ?>
                   <h2><?php print $title ?></h2>
                 <?php endif; ?>
-
-                <?php print $messages; ?>
                 <?php print render($page['content']); ?>
-                
             </div>
             <?php if (!empty($page['sidebar_first'])): ?>
             <div id="right_column">
@@ -48,5 +38,23 @@
         </div>
         
     </div>
+</section>
+<?php else: ?>
+<section class="std_content">
+  <div class="wrapper_2 other after_clear">
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <div class="c_left">
+        <?php print render($page['sidebar_first']); ?>
+      </div>
+    <?php endif; ?>
+
+    <div class="c_right">
+      <?php print $messages; ?>
+      <?php if ($title): ?>
+        <h2><?php print $title ?></h2>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </div>
+  </div>
 </section>
 <?php endif; ?>

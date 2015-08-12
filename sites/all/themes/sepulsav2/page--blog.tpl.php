277b324d644a769fe9dc9a75357de335e74936ec
ekkypatria <ekkypatria@gmail.com>
2015-08-12 11:41:23 +0700

    blog sepulsa

--- sites/all/themes/sepulsav2/page--blog.tpl.php
+++ sites/all/themes/sepulsav2/page--blog.tpl.php
@@ -11,7 +11,7 @@ global $base_url;
 $theme_path = $base_url . '/' . path_to_theme();
 ?>
 <section class="banner sblog">
-    <img src="<?php print $theme_path; ?>/images/content/banner_blog.jpg" alt="blog" />
+    <img src="<?php print $theme_path; ?>/images/content/banner_blog.jpg" alt="sepulsa blog" />
     <div class="caption">
         <h2><?php print t('Berbagi Info Menarik dan Tips Bermanfaat.'); ?></h2>
         <p><?php print t('Baca Info Menarik dan Tips Bermanfaat Seputar Pulsa di Sepulsa Blog.'); ?></p>

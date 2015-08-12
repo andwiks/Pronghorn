3c51e4731df6a76e0422939913ad38d9645a542a
Dwi Susanto <kansertjr@gmail.com>
2015-08-12 09:39:56 +0700

    multilanguage support

--- sites/all/themes/sepulsav2/inc_header.php
+++ sites/all/themes/sepulsav2/inc_header.php
@@ -12,7 +12,7 @@ global $base_url, $user;
 ?>
     <header>
         <div class="wrapper">
-            <a href="javascript:;" onclick="buynow()" class="belipulsa">Beli Pulsa</a>
+            <a href="javascript:;" onclick="buynow()" class="belipulsa"><?php print t('Beli Pulsa'); ?></a>
 
             <div class="soc_link">
                 <a href="https://www.facebook.com/sepulsa" class="fb">facebook</a>
@@ -29,33 +29,33 @@ global $base_url, $user;
                     <div class="box myacc">
                         <a href="" >
                             <span class="ico"></span>
-                            My Acc
+                            <?php print t('My Acc'); ?>
                         </a>
                         <div class="box_drop">
                             <div class="arr"></div>
                             <div class="box_credit after_clear">
-                                <div class="left">SEPULSA CREDIT</div>
+                                <div class="left"><?php print t('SEPULSA CREDIT'); ?></div>
                                 <div class="right"><?php print number_format(userpoints_get_current_points($user->uid, 'all'), 0, ",", "."); ?> IDR</div>
                             </div>
-                            <a href="<?php print url('user/' . $user->uid); ?>">AKUN</a>
-                            <a href="<?php print url("konfirmasi"); ?>">KONFIRMASI PEMBAYARAN</a>
-                            <a href="<?php print url("user/logout"); ?>">LOG OUT</a>
+                            <a href="<?php print url('user/' . $user->uid); ?>"><?php print t('AKUN'); ?></a>
+                            <a href="<?php print url("konfirmasi"); ?>"><?php print t('KONFIRMASI PEMBAYARAN'); ?></a>
+                            <a href="<?php print url("user/logout"); ?>"><?php print t('KELUAR'); ?></a>
                         </div>
                     </div>
                     <div class="nav_mobile">
                         <a href="" class="toggle">toogle</a>
                         <div class="box_drop">
                             <div class="box_credit after_clear">
-                                <div class="left">SEPULSA CREDIT</div>
+                                <div class="left"><?php print t('SEPULSA CREDIT'); ?></div>
                                 <div class="right"><?php print commerce_currency_format(userpoints_get_current_points($user->uid, 'all'), 'IDR'); ?></div>
                             </div>
-                            <a href="<?php print url('user/' . $user->uid); ?>" >Informasi Akun</a>
-                            <a href="<?php print url('user/orders'); ?>">Order Transaksi</a>
-                            <a href="<?php print url('user/token'); ?>">Info Kartu</a>
-                            <a href="<?php print url('user/wallet'); ?>">Dompet Saya</a>
-                            <a href="akun_voucher.php">Voucher Diskon</a>
-                            <a href="<?php print url("konfirmasi"); ?>">Konfirmasi Pembayaran</a>
-                            <a href="<?php print url("user/logout"); ?>">LOG OUT</a>
+                            <a href="<?php print url('user/' . $user->uid); ?>" ><?php print t('Informasi Akun'); ?></a>
+                            <a href="<?php print url('user/orders'); ?>"><?php print t('Order Transaksi'); ?></a>
+                            <a href="<?php print url('user/token'); ?>"><?php print t('Info Kartu'); ?></a>
+                            <a href="<?php print url('user/wallet'); ?>"><?php print t('Dompet Saya'); ?></a>
+                            <a href="akun_voucher.php"><?php print t('Voucher Diskon'); ?></a>
+                            <a href="<?php print url("konfirmasi"); ?>"><?php print t('Konfirmasi Pembayaran'); ?></a>
+                            <a href="<?php print url("user/logout"); ?>"><?php print t('KELUAR'); ?></a>
                             <div class="m_lang">
                                 <a href="">English</a>
                                 <a href="">Indonesia</a>

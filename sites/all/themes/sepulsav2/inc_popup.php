4ed28e29ac0e426ceba8f965c34bffe6008509b5
Dwi Susanto <kansertjr@gmail.com>
2015-08-13 15:25:54 +0700

    offline voucher popup

--- sites/all/themes/sepulsav2/inc_popup.php
+++ sites/all/themes/sepulsav2/inc_popup.php
@@ -24,33 +24,3 @@
         </div>
     </div>
 </div>
-<div class="wrap_popup voucher" id="detail_voucher">
-    <div class="box_popup">
-        <a href="" class="close style_1"><img src="<?php print $theme_path; ?>/images/material/close_popup.png" alt="close" /></a>
-        <div class="content_pop">
-            <h3></h3>
-            <div class="image">
-            </div>
-            <h4></h4>
-
-            <div class="redeem">
-                <h6><?php print t('REEDEM Voucher kamu'); ?></h6>
-                <span>
-                    <?php print t('Kode akan dicheck dan diisi oleh pihak merchant yang tertera di kupon. Tunjukan halaman ini, dan reedem voucher kamu.'); ?>
-                </span>
-                <div class="form">
-                  <?php $form = drupal_get_form('offline_coupon_redeem_form', 0); ?>
-                  <?php print drupal_render($form); ?>
-                </div>
-            </div>
-
-            <h5></h5>
-            <p>
-            </p>
-        </div>
-        <div class="nav_pop">
-            <a href="" class="prev"><</a> 
-            <a href="" class="next">></a> 
-        </div>
-    </div>
-</div>

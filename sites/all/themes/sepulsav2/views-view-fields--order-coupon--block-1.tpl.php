9c416d9e712c385f21566fcc9cf41aad8d1bd8ee
ekkypatria <ekkypatria@gmail.com>
2015-08-14 20:27:29 +0700

    styling coupon on complete page

--- sites/all/themes/sepulsav2/views-view-fields--order-coupon--block-1.tpl.php
+++ sites/all/themes/sepulsav2/views-view-fields--order-coupon--block-1.tpl.php
@@ -1,6 +1,6 @@
 <img src="<?php print $fields['field_coupon_product_image']->content; ?>" alt="">
-<div class="shortcode-banner-content">
-  <h4 class="banner-title">Kupon kamu sudah dapat digunakan di <strong><a href="http://<?php print $fields['field_website']->content; ?>" target="_blank"><?php print $fields['field_website']->content; ?></a></strong>.
+<div class="shortcode-banner-content" style="margin-top: 15px; font-size: 14px;">
+  <h4 class="banner-title" style="font-size: 14px;">Kupon kamu sudah dapat digunakan di <strong><a href="http://<?php print $fields['field_website']->content; ?>" target="_blank"><?php print $fields['field_website']->content; ?></a></strong>.
     <br /><br />
     <?php print $fields['field_detail_simple_coupon']->content; ?>
   </h4>

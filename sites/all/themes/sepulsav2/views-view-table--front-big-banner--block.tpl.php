476267bcf8666beb1038fe3e8ee1430d2c74e716
ekkypatria <ekkypatria@gmail.com>
2015-08-18 12:00:14 +0700

    front banner

--- sites/all/themes/sepulsav2/views-view-table--front-big-banner--block.tpl.php
+++ sites/all/themes/sepulsav2/views-view-table--front-big-banner--block.tpl.php
@@ -4,7 +4,10 @@
         <?php foreach ($rows as $row_count => $row): ?>
             <div class="slide">
                 <?php print $row['field_image_banner']; ?>
-                <?php print $row['field_link_title']; ?>
+                <div class="slide_content">
+                    <?php print $row['field_link_title']; ?>
+                </div>
+                
             </div>
         <?php endforeach; ?>
         </div>

9192438a792dab4c313a24b51056f6d2b0271dc9
Tri <tri@reebonz.com>
2015-09-08 10:11:19 +0700

    download csv report

--- sites/all/modules/custom/referral_sepulsa/daily_referral_report.tpl.php
+++ sites/all/modules/custom/referral_sepulsa/daily_referral_report.tpl.php
@@ -2,7 +2,13 @@
 $count = 1;
 $form = drupal_get_form('admin_daily_referral_report_form');
 print drupal_render($form);
+
+if (isset($_GET['sd']) && isset($_GET['ed']))
+	$download = l('Download CSV', 'admin/people/referral/daily_referral_report/csv', array("query" => array("sd" => $_GET['sd'], "ed" => $_GET['ed'])));
+else
+	$download = l('Download CSV', 'admin/people/referral/daily_referral_report/csv');
 ?>
+<div style="float: right;"><?php print $download ?></div>
 <table>
 	<thead>
 		<tr>

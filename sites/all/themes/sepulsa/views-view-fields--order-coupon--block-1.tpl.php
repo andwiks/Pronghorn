86c0e70451e87dfc1831bab727d17871a93eef95
andreyunugro <andre4s_y@yahoo.com>
2016-01-28 10:22:03 +0700

    Remove old sepulsa theme.

--- sites/all/themes/sepulsa/views-view-fields--order-coupon--block-1.tpl.php
+++
@@ -1,7 +0,0 @@
-<img src="<?php print $fields['field_coupon_product_image']->content; ?>" alt="">
-<div class="shortcode-banner-content">
-  <h4 class="banner-title">Kupon kamu sudah dapat digunakan di <strong><a href="http://<?php print $fields['field_website']->content; ?>" target="_blank"><?php print $fields['field_website']->content; ?></a></strong>.
-    <br /><br />
-    <?php print $fields['field_detail_simple_coupon']->content; ?>
-  </h4>
-</div>

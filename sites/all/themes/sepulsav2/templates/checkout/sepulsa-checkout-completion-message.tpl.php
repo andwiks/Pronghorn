06335e9e049998ff8a7a333ea76c34e0a0b122b1
andreyunugro <andre4s_y@yahoo.com>
2016-02-07 21:02:38 +0700

    Update checkout complete page template.

--- sites/all/themes/sepulsav2/templates/checkout/sepulsa-checkout-completion-message.tpl.php
+++ sites/all/themes/sepulsav2/templates/checkout/sepulsa-checkout-completion-message.tpl.php
@@ -12,8 +12,7 @@ if (module_exists('facebook_twitter_share')) {
 
 <section class="transaction">
     <div class="wrapper_3 thanks_page after_clear ">
-        <h2>Transaksi Anda Sukses</h2>
-        <h5>Terima kasih telah mempercayakan Sepulsa sebagai dealer pulsa kamu!</h5>
+        <h2>TERIMA KASIH ATAS TRANSAKSI ANDA</h2>
         <?php if (module_exists('facebook_twitter_share') && count($returned_facebook_twitter_share) > 0) { ?>
         <div class="facebook_twitter_share_container">
             <div class="facebook_twitter_share_description">
@@ -86,43 +85,7 @@ if (module_exists('facebook_twitter_share')) {
         </table>
         <button name="nps-submit" type="button">Vote</button>
         </div>
-        <?php
-        //if bank transfer
-        /*
-        <div class="c_left">
-            <h4>
-                <img src="<?php print url(path_to_theme()); ?>/images/material/bank_transfer_info.png" alt="img bank" />
-                BANK TRANSFER INFO
-            </h4>
-            <div class="checkout_complete">
-            	<?php print $message; ?>
-        	</div>
-        </div>
-        <div class="c_right">
-            <h4>
-                <img src="<?php print url(path_to_theme()); ?>/images/material/informasi_penting.png" alt="img info" />
-                INFORMASI PENTING
-            </h4>
-            <p>
-                Mohon lakukan pembayaran melalui internet/mobile banking atau melalui teller di bank pilihan kamu ke akun berikut dalam jangka waktu <span>kurang dari 24 jam</span>. Jika tidak pesanan kamu akan dibatalkan.
-                <br/><br/>
-                Untuk tahap akhir proses pembayaran, silakan lakukan konfirmasi pembayaran dengan melakukan konfirmasi pembayaran <a href="<?php print url('konfirmasi'); ?>">disini</a> agar transaksi kamu dapat langsung kami proses.
-                <br/><br/>
-                Pulsa kamu akan terisi setelah pembayaran sukses dan dikonfirmasi.
-                <br/><br/>
-                Jika ini adalah transaksi pertama kamu, silahkan cek email kamu untuk mendapatkan instruksi login. Setelah login, kamu bisa melihat status pulsa kamu di halaman <a href="<?php print url('user'); ?>">akun saya</a>.
-            </p>
-        </div>
 
-    	<?php
-        //if not bank transfer
-        /*
-        	<div class="cc_thankyou" style="width:100%">
-        		print $message;
-        	</div>
-    	*/
-        ?>
-		
 		<?php print $message; ?>
 
     </div>

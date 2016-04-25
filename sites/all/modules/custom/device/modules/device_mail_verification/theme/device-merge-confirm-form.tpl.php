a3ec56b026090da3a922597da6144700aaf23b15
andreyunugro <andre4s_y@yahoo.com>
2016-04-25 16:46:34 +0700

    [Start #118161587] add custom theme for page device mail verification.

---
+++ sites/all/modules/custom/device/modules/device_mail_verification/theme/device-merge-confirm-form.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * device-merge-confirm-form.tpl.php
+ *
+ * @author andreas@sepulsa.com
+ */
+?>
+<div style="text-align: center;">
+  <?php print drupal_render_children($form); ?>
+</div>

98c6f26405a156abc56c8be88c71e74a0e1ee723
azul <azulkipli@gmail.com>
2016-04-28 17:39:27 +0700

    [115921157] expand current category on faq detail

--- sites/all/themes/sepulsav2/templates/views-view-unformatted--faqs--block-2.tpl.php
+++ sites/all/themes/sepulsav2/templates/views-view-unformatted--faqs--block-2.tpl.php
@@ -6,8 +6,21 @@
  *
  * @ingroup views_templates
  */
+  // kpr(get_defined_vars());
+$rowclass = 'deactive';
+if ($node = menu_get_object()) {
+ if ($node->field_faq_category['und'][0]['taxonomy_term']->name == $title ) {
+  $rowclass = 'active';
+ }
+
+}
+
+  foreach ($rows as $id => $row){
+
+  }
+
 ?>
-<div class="row accordion deactive">
+<div class="row accordion <?php print $rowclass ?>">
   <?php if (!empty($title)): ?>
     <div class="label"><span class="ico">+</span> <?php print $title; ?></div>
   <?php endif; ?>

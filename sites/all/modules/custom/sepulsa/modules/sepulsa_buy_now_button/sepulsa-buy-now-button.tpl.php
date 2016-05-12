f5c821b764e48312ec4688c7a37cc2447cae8470
nuraizatif <nuraizatif@gmail.com>
2016-05-12 14:06:08 +0700

    [#118365369] edit link in tpl

--- sites/all/modules/custom/sepulsa/modules/sepulsa_buy_now_button/sepulsa-buy-now-button.tpl.php
+++ sites/all/modules/custom/sepulsa/modules/sepulsa_buy_now_button/sepulsa-buy-now-button.tpl.php
@@ -47,7 +47,7 @@ if (empty($utm_link)) {
   $utm_link = '/';
 }
 else {
-  $utm_link .= '/?' . $utm_link;
+  $utm_link = '/?' . $utm_link;
 }
 ?>
 <a href="<?php echo $utm_link ?>" class="bt_std bt_beli">

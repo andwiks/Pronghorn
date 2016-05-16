f500a225119b3ddd497023ee6b5bc6394516ee3f
nuraizatif <nuraizatif@gmail.com>
2016-05-16 10:13:07 +0700

    [#118365369] revisi , change utm button link method

--- sites/all/modules/custom/sepulsa/modules/sepulsa_buy_now_button/sepulsa-buy-now-button.tpl.php
+++ sites/all/modules/custom/sepulsa/modules/sepulsa_buy_now_button/sepulsa-buy-now-button.tpl.php
@@ -3,53 +3,45 @@
  * @file
  * Sepulsa Buy Now Button TPL.
  */
-$utm_link = '';
-if (isset($_COOKIE['utm_source_cookie']) && !empty($_COOKIE['utm_source_cookie'])) {
-  $utm_link .= 'utm_source=' . $_COOKIE['utm_source_cookie'];
+
+$array_query = array();
+if (isset($_COOKIE['utm_source_cookie']) &&
+  !empty($_COOKIE['utm_source_cookie'])) {
+  $array_query['utm_source'] = $_COOKIE['utm_source_cookie'];
 }
 else {
-  if (isset($data['sepulsa_buy_now_button_utm_source']) && !empty($data['sepulsa_buy_now_button_utm_source'])) {
-    $utm_link .= 'utm_source=' . $data['sepulsa_buy_now_button_utm_source'];
+  if (isset($data['sepulsa_buy_now_button_utm_source']) &&
+    !empty($data['sepulsa_buy_now_button_utm_source'])) {
+    $array_query['utm_source'] = $data['sepulsa_buy_now_button_utm_source'];
   }
 }
 
-if (isset($_COOKIE['utm_medium_cookie']) && !empty($_COOKIE['utm_medium_cookie'])) {
-  if (!empty($utm_link)) {
-    $utm_link .= '&';
-  }
-  $utm_link .= 'utm_medium=' . $_COOKIE['utm_medium_cookie'];
+if (isset($_COOKIE['utm_medium_cookie']) &&
+  !empty($_COOKIE['utm_medium_cookie'])) {
+  $array_query['utm_medium'] = $_COOKIE['utm_medium_cookie'];
 }
 else {
-  if (isset($data['sepulsa_buy_now_button_utm_medium']) && !empty($data['sepulsa_buy_now_button_utm_medium'])) {
-    if (!empty($utm_link)) {
-      $utm_link .= '&';
-    }
-    $utm_link .= 'utm_medium=' . $data['sepulsa_buy_now_button_utm_medium'];
+  if (isset($data['sepulsa_buy_now_button_utm_medium']) &&
+    !empty($data['sepulsa_buy_now_button_utm_medium'])) {
+    $array_query['utm_medium'] = $data['sepulsa_buy_now_button_utm_medium'];
   }
 }
 
-if (isset($_COOKIE['utm_campaign_cookie']) && !empty($_COOKIE['utm_campaign_cookie'])) {
-  if (!empty($utm_link)) {
-    $utm_link .= '&';
-  }
-  $utm_link .= 'utm_campaign=' . $_COOKIE['utm_campaign_cookie'];
+if (isset($_COOKIE['utm_campaign_cookie']) &&
+  !empty($_COOKIE['utm_campaign_cookie'])) {
+  $array_query['utm_campaign'] = $_COOKIE['utm_campaign_cookie'];
 }
 else {
-  if (isset($data['sepulsa_buy_now_button_utm_campaign']) && !empty($data['sepulsa_buy_now_button_utm_campaign'])) {
-    if (!empty($utm_link)) {
-      $utm_link .= '&';
-    }
-    $utm_link .= 'utm_campaign=' . $data['sepulsa_buy_now_button_utm_campaign'];
+  if (isset($data['sepulsa_buy_now_button_utm_campaign']) &&
+    !empty($data['sepulsa_buy_now_button_utm_campaign'])) {
+    $array_query['utm_campaign'] = $data['sepulsa_buy_now_button_utm_campaign'];
   }
 }
 
-if (empty($utm_link)) {
-  $utm_link = '/';
-}
-else {
-  $utm_link = '/?' . $utm_link;
-}
 ?>
-<a href="<?php echo $utm_link ?>" class="bt_std bt_beli">
-  <?php echo t('BELI PULSA SEKARANG')?>
-</a>
+<?php echo l(t('BELI PULSA SEKARANG'),
+  '',
+  array(
+    'query' => $array_query,
+    'attributes' => array('class' => 'bt_std bt_beli'),
+  ))?>

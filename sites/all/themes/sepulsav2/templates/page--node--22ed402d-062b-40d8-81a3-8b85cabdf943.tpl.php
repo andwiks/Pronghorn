52cd26b2bb540f0358c6b52412ccd3eca46579e8
ekky <ekky@sepulsa.com>
2016-05-17 13:06:03 +0700

    gue lupa command, tapi ini done (tinggal ubah di webformnya aja)

--- sites/all/themes/sepulsav2/templates/page--node--22ed402d-062b-40d8-81a3-8b85cabdf943.tpl.php
+++ sites/all/themes/sepulsav2/templates/page--node--22ed402d-062b-40d8-81a3-8b85cabdf943.tpl.php
@@ -11,19 +11,27 @@
 
 
 <section class="std_content">
+
   <div class="wrapper_2 other after_clear">
+    <?php print $messages; ?>
+  </div>
+  
+  <div class="wrapper_2 other after_clear uob-campaign">
     <?php if (!empty($page['sidebar_first'])): ?>
       <div class="w3-col l6 m6 s12">
-        <?php print render($page['sidebar_first']); ?>
+        <div class="uob-snk">
+          <?php print render($page['sidebar_first']); ?>
+        </div>
       </div>
     <?php endif; ?>
 
     <div class="w3-col l6 m6 s12">
-      <?php print $messages; ?>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
+      <div class="uob-form">
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+      </div>
     </div>
   </div>
 </section>
